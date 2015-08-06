3d23e5ea2cc9733000b37387cb3b9a796ced71c6
tuber <btu@thoughtworks.com>
2015-08-06 20:30:39 +0800

    [tuber] add services

--- app/src/main/java/com/thoughtworks/btu/luckyxx/LaunchActivity.java
+++ app/src/main/java/com/thoughtworks/btu/luckyxx/LaunchActivity.java
@@ -1,15 +1,15 @@
 package com.thoughtworks.btu.luckyxx;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Build;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.Switch;
 
 
-public class LaunchActivity extends ActionBarActivity {
+public class LaunchActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +24,14 @@ public class LaunchActivity extends ActionBarActivity {
         } else {
             notification.setVisibility(View.GONE);
         }
+    }
+
+    public void handleFetchLuckyMoneyButtonClicked(View view) {
+        startActivity(new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS));
+    }
+
+    public void handleNotificationButtonClicked(View view) {
+        startActivity(new Intent(Settings.ACTION_NOTIFICATION_LISTENER_SETTINGS));
     }
 
 }
